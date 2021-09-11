09b51f50057335aa4c1028f4e575e7ad7d2bc404
Taegan Loyzelle <tloyzelle@gmail.com>
2021-09-11 16:54:07 -0400

    adding useEffect

--- src/App.js
+++ src/App.js
@@ -47,4 +47,78 @@ const App = () => {
             console.log(err);
         }
     };
+
+    useEffect(() => {
+        fetchEmployees();
+    }, []);
+
+    const searchHandler = (search) => {
+        console.log("Search", search);
+        setSearch(search);
+
+        if (search !== "") {
+            const newEmployeeList = employees.filter((employee) => {
+                return (
+                    Object.values(employee)
+                    .toLowerCase()
+                    .includes(search.toLowerCase())
+                );
+            });
+            setSearchResults(newEmployeeList);
+        } else {
+            setSearchResults(employees);
+        }
+    };
+
+    useEffect(() => {
+        const getAllEmployees = async () => {
+            const allEmployees = await fetchEmployees();
+            if (allEmployees) setEmployees(allEmployees);
+        };
+        getAllEmployees();
+    }, []);
+
+    useEffect(() => {}, [Employees]);
+
+    if (loading) {
+        return (
+            <Wrapper>
+                <Loading />
+            </Wrapper>
+        );
+    }
+
+    return (
+        <Wrapper>
+            <div className = "container">
+                <div className = "row">
+                    <Col size = "md-4">
+                        <h2>Employee Directory</h2>
+
+                        <SearchForm
+                        searchKeyword = {searchHandler}
+                        term = {search}
+                         />
+                    </Col>
+                </div>
+
+                <div className = "row">
+                    <Col size = "md-12">
+                        <table clawsName = "table">
+                            <thead>
+                                <tr>
+                                    <th>Photo</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>City</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </Col>
+                </div>
+            </div>
+        </Wrapper>
+    )
 }
